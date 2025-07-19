402e090d7992e1071014b71027c325a74903d101
Gustavo Sobral <gustavo.b.sobral@gmail.com>
2025-07-19 12:27:58 +0100

    Aula 19 Django
    Restrigindo páninas para quem não está logado

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Url da pagina de login
+LOGIN_URL = '/users/login'
